0bde848c48dfa9bc178195aa9ee401da620b9546
francky <edia.francky@gmail.com>
2022-09-27 14:35:36 +0200

    fill DOM with values

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const apiKey = 'cda612fc39faed4b537256e2db2f****';
+const iconHTML = document.getElementById('weather-icon');
+const loc = document.querySelector('.location');
+const desc = document.querySelector('.desc');
+const tempC = document.querySelector('.c');
+const sunriseHTML = document.querySelector('.sunrise');
+const sunsetHTML = document.querySelector('.sunset');
 
 window.addEventListener('load', () => {
     let lat;
@@ -10,15 +16,29 @@ window.addEventListener('load', () => {
             lat = position.coords.latitude;
             long = position.coords.longitude;
             const apiBase = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-            console.log(apiBase);
+
             fetch(apiBase)
-                .then( (response) => response.json() )
+                .then( (response) => {
+                    return response.json();
+                })
                 .then( (data) => {
+                    console.log(data);
                     const location = data.name;
                     const {temp} = data.main;
                     const {description, icon} = data.weather[0];
                     const {sunrise, sunset} = data.sys;
-                    console.log(description);
+
+                    const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+                    const sunriseGMT = new Date(sunrise * 1000);
+                    const sunsetGMT = new Date(sunset * 1000);
+
+                    iconHTML.src = iconUrl;
+                    loc.textContent = `${location}`;
+                    desc.textContent = `${description}`;
+                    tempC.textContent = `${temp.toFixed(1)} °C`;
+                    sunriseHTML.textContent = `${sunriseGMT.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'} )}`;
+                    sunsetHTML.textContent = `${sunsetGMT.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'} )}`;
                 })
         })
     }
