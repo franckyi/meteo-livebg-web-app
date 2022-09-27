caa12f065c557c96e0694964f328a0367261a9b9
francky <edia.francky@gmail.com>
2022-09-27 10:24:09 +0200

    obtain user position

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const apiKey = 'cda612fc39faed4b537256e2db2f****';
 
 window.addEventListener('load', () => {
+    let lat;
+    let long;
 
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+        })
+    }
 });
