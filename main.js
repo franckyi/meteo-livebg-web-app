5777d1134b5cf3adc9e3e9e44ddcfe8b59672170
francky <edia.francky@gmail.com>
2022-09-27 11:53:11 +0200

    fetch and destructurize

--- main.js
+++ main.js
@@ -9,6 +9,20 @@ window.addEventListener('load', () => {
             console.log(position);
             lat = position.coords.latitude;
             long = position.coords.longitude;
+            const apiBase = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+            console.log(apiBase);
+            fetch(apiBase)
+                .then( (response) => response.json() )
+                .then( (data) => {
+                    const location = data.name;
+                    const {temp} = data.main;
+                    const {description, icon} = data.weather[0];
+                    const {sunrise, sunset} = data.sys;
+                    console.log(description);
+                })
         })
     }
+
+
+
 });
