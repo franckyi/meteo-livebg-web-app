1503b623828bf646a56aa8d095434566ffd2ba08
Francky <contact@francky.pl>
2024-02-06 15:22:58 +0100

    use env

---
+++ js/api-variables.js
@@ -0,0 +1,2 @@
+export const OPENWEATHER_APIKEY = process.env.OPENWEATHER_APIKEY;
+export const PEXELS_APIKEY = process.env.PEXELS_APIKEY;
--- js/background.js
+++ js/background.js
@@ -1,35 +1,34 @@
-import { PEXELS_APIKEY } from '../js/keys.js';
+import { PEXELS_APIKEY } from "../js/api-variables.js";
 
 function replaceBackground(data) {
-    let randomIndex = Math.floor( Math.random() * ((data.photos.length)+1) );
-    let imageUrl;
-    let h = window.innerHeight;
-    let w = window.innerWidth;
+  let randomIndex = Math.floor(Math.random() * (data.photos.length + 1));
+  let imageUrl;
+  let h = window.innerHeight;
+  let w = window.innerWidth;
 
-    if (h > w) {
-        imageUrl = data.photos[randomIndex].src.portrait;
-    }
-    else if (h < w) {
-        imageUrl = data.photos[randomIndex].src.landscape;
-    }
-    if (typeof imageUrl === 'undefined') {
-        document.body.style.backgroundImage = 'url(' + '../images/fb.jpg' + ')';
-    }
-    else {
-        document.body.style.backgroundImage = 'url(' + imageUrl + ')';
-    }   
+  if (h > w) {
+    imageUrl = data.photos[randomIndex].src.portrait;
+  } else if (h < w) {
+    imageUrl = data.photos[randomIndex].src.landscape;
+  }
+  if (typeof imageUrl === "undefined") {
+    document.body.style.backgroundImage = "url(" + "../images/fb.jpg" + ")";
+  } else {
+    document.body.style.backgroundImage = "url(" + imageUrl + ")";
+  }
 }
 
 function fetchImage(query) {
-    fetch(`https://api.pexels.com/v1/search?query=${query}`,
-    { headers: { Authorization: PEXELS_APIKEY } } )
-    .then( resp => resp.json() )
-    .then( data => {
-        replaceBackground(data);
-    })
+  fetch(`https://api.pexels.com/v1/search?query=${query}`, {
+    headers: { Authorization: PEXELS_APIKEY },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      replaceBackground(data);
+    });
 }
 
 export const chooseWord = function (temp, description) {
-    console.warn(temp, description);
-    fetchImage(description);
-}
+  console.warn(temp, description);
+  fetchImage(description);
+};
